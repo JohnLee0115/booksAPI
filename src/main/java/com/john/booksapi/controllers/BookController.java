66506a7dd4e6ff3964962e6067cf6dd61f7a3dc4
JohnLee0115 <johnlee011500@gmail.com>
2023-08-14 03:19:50 -0700

    Rendering_All_Books

--- src/main/java/com/john/booksapi/controllers/BookController.java
+++ src/main/java/com/john/booksapi/controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.john.booksapi.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.john.booksapi.models.Book;
 import com.john.booksapi.services.BookService;
 
 @Controller
@@ -14,6 +17,14 @@ public class BookController {
 	
 	public BookController (BookService bookService) {
 		this.bookService = bookService;
+	}
+	
+	@RequestMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		
+		return "/books/index.jsp";
 	}
 	
 	@RequestMapping("/books/{id}")
